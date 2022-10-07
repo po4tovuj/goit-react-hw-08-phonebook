4833f374f460af10fc17055784a99812121f595d
Sergey Miniaylo <po4tovuj@gmail.com>
2022-10-07 17:16:23 +0300

    handle contacts with local storage

--- src/components/App/index.jsx
+++ src/components/App/index.jsx
@@ -7,14 +7,19 @@ import shortid from 'shortid';
 const INITIAL_STATE = {
   contacts: [],
 };
+const CONTACTS = 'CONTACTS';
 export class App extends Component {
   state = {
     ...INITIAL_STATE,
   };
   handleDelete = id => {
+    const updatedContacts = this.state.contacts.filter(
+      contact => contact.id !== id
+    );
     this.setState(state => ({
-      contacts: state.contacts.filter(contact => contact.id !== id),
+      contacts: updatedContacts,
     }));
+    localStorage.setItem(CONTACTS, JSON.stringify(updatedContacts));
   };
   onSubmit = contact => {
     const isAlreadyExist = this.state.contacts.find(
@@ -23,15 +28,30 @@ export class App extends Component {
     if (isAlreadyExist) {
       throw new Error(`${contact.name} is already in contacts`);
     }
+    const newContact = { ...contact, id: shortid.generate() };
+    this.addContactToLocalStorage(newContact);
     this.setState(prevState => {
       return {
-        contacts: [
-          ...prevState.contacts,
-          { ...contact, id: shortid.generate() },
-        ],
+        contacts: [...prevState.contacts, newContact],
       };
     });
   };
+  getContactFromStorage = () => {
+    const contacts = localStorage.getItem(CONTACTS);
+    return contacts ? JSON.parse(contacts) : [];
+  };
+  addContactToLocalStorage = contact => {
+    let savedContacts = localStorage.getItem(CONTACTS);
+    savedContacts = savedContacts ? JSON.parse(savedContacts) : [];
+    console.log('savedContacts: ', savedContacts);
+    savedContacts.push(contact);
+    console.log('after savedContacts: ', savedContacts);
+    localStorage.setItem(CONTACTS, JSON.stringify(savedContacts));
+  };
+
+  componentDidMount() {
+    this.setState({ contacts: this.getContactFromStorage() });
+  }
   render() {
     return (
       <Container>
